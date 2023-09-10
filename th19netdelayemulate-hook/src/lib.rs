cb091f5c4dd82ffa6cf4584e339b35c5b099b17b
progre <progre+pgp@prgrssv.net>
2023-09-10 23:45:59 +0900

    feat(netdelayemulate): change delay

--- th19netdelayemulate-hook/src/lib.rs
+++ th19netdelayemulate-hook/src/lib.rs
@@ -1,6 +1,13 @@
-use std::ffi::c_void;
+use std::{
+    cmp::Ordering,
+    ffi::{c_void, OsStr},
+    io::Read,
+    sync::mpsc,
+    thread::spawn,
+};
 
 use bytes::{Buf, BufMut, BytesMut};
+use interprocess::os::windows::named_pipe::{ByteReaderPipeStream, PipeListenerOptions, PipeMode};
 use windows::Win32::{
     Foundation::HINSTANCE,
     System::{Console::AllocConsole, SystemServices::DLL_PROCESS_ATTACH},
@@ -10,6 +17,7 @@ use junowen::Th19;
 
 static mut TH19: Option<Th19> = None;
 static mut STATE: Option<State> = None;
+static mut NEW_DELAY_RECIEVER: Option<mpsc::Receiver<i8>> = None;
 
 struct State {
     p1_buffer: BytesMut,
@@ -18,17 +26,9 @@ struct State {
 
 impl State {
     fn new() -> Self {
-        let mut p1_buffer = BytesMut::with_capacity(2 * 4);
-        for _ in 0..(p1_buffer.capacity() / 4) {
-            p1_buffer.put_u32(0);
-        }
-        let mut p2_buffer = BytesMut::with_capacity(2 * 4);
-        for _ in 0..(p2_buffer.capacity() / 4) {
-            p2_buffer.put_u32(0);
-        }
         Self {
-            p1_buffer,
-            p2_buffer,
+            p1_buffer: BytesMut::new(),
+            p2_buffer: BytesMut::new(),
         }
     }
 }
@@ -41,26 +41,69 @@ fn state_mut() -> &'static mut State {
     unsafe { STATE.as_mut().unwrap() }
 }
 
+fn new_delay_receiver() -> &'static mpsc::Receiver<i8> {
+    unsafe { NEW_DELAY_RECIEVER.as_ref().unwrap() }
+}
+
 extern "fastcall" fn hook_0abb2b() -> u32 {
     let th19 = th19();
     let state = state_mut();
 
     let input = th19.input_mut();
 
-    let p1_idx = input.p1_input_idx;
-    let old_p1 = state.p1_buffer.get_u32();
-    let p1 = input.input_device_array[p1_idx as usize].input;
-    input.input_device_array[p1_idx as usize].input = old_p1;
-    state.p1_buffer.put_u32(p1);
+    let new_delay_receiver = new_delay_receiver();
+    if let Ok(delay) = new_delay_receiver.try_recv() {
+        let old_delay = state.p1_buffer.len() / 4;
+        println!("old delay: {}, new delay: {}", old_delay, delay);
+        let delay = delay as usize;
+        match delay.cmp(&old_delay) {
+            Ordering::Less => {
+                let skip = (old_delay - delay) * 4;
+                state.p1_buffer.advance(skip);
+                state.p2_buffer.advance(skip);
+            }
+            Ordering::Greater => {
+                for _ in 0..(delay - old_delay) {
+                    state.p1_buffer.put_u32(0);
+                    state.p2_buffer.put_u32(0);
+                }
+            }
+            Ordering::Equal => (),
+        }
+    }
 
-    let p2_idx = input.p2_input_idx;
-    let old_p2 = state.p2_buffer.get_u32();
-    let p2 = input.input_device_array[p2_idx as usize].input;
-    input.input_device_array[p2_idx as usize].input = old_p2;
-    state.p2_buffer.put_u32(p2);
+    let p1_idx = input.p1_input_idx;
+    if !state.p1_buffer.is_empty() {
+        let old_p1 = state.p1_buffer.get_u32();
+        let p1 = input.input_device_array[p1_idx as usize].input;
+        input.input_device_array[p1_idx as usize].input = old_p1;
+        state.p1_buffer.put_u32(p1);
+
+        let p2_idx = input.p2_input_idx;
+        let old_p2 = state.p2_buffer.get_u32();
+        let p2 = input.input_device_array[p2_idx as usize].input;
+        input.input_device_array[p2_idx as usize].input = old_p2;
+        state.p2_buffer.put_u32(p2);
+    }
 
     // p1 の入力を返す
     input.input_device_array[p1_idx as usize].input
+}
+
+fn init_interprecess(tx: mpsc::Sender<i8>) {
+    let pipe = PipeListenerOptions::new()
+        .name(OsStr::new("th19netdelayemulate"))
+        .mode(PipeMode::Bytes)
+        .create()
+        .unwrap();
+
+    let mut buf = [0; 1];
+    spawn(move || loop {
+        let mut reader: ByteReaderPipeStream = pipe.accept().unwrap();
+        reader.read_exact(&mut buf).unwrap();
+        println!("pipe received {}", buf[0]);
+        tx.send(buf[0] as i8).unwrap();
+    });
 }
 
 #[no_mangle]
@@ -75,8 +118,13 @@ pub extern "stdcall" fn DllMain(
         }
         let th19 = Th19::new_hooked_process("th19.exe").unwrap();
         th19.hook_0abb2b(hook_0abb2b as _).unwrap();
-        unsafe { TH19 = Some(th19) };
-        unsafe { STATE = Some(State::new()) };
+        let (tx, rx) = mpsc::channel();
+        init_interprecess(tx);
+        unsafe {
+            TH19 = Some(th19);
+            STATE = Some(State::new());
+            NEW_DELAY_RECIEVER = Some(rx);
+        }
     }
     true
 }
--- th19netdelayemulate/src/main.rs
+++ th19netdelayemulate/src/main.rs
@@ -1,16 +1,33 @@
-use std::env::current_exe;
+use std::{
+    env::{self, current_exe},
+    ffi::OsStr,
+    io::Write,
+};
 
 use anyhow::Result;
+use interprocess::os::windows::named_pipe::ByteWriterPipeStream;
 use junowen::inject_dll::inject_dll;
 
 fn main() -> Result<()> {
-    let dll_path = current_exe()?
-        .as_path()
-        .parent()
-        .unwrap()
-        .join(concat!(env!("CARGO_PKG_NAME"), "_hook.dll"));
+    let name = OsStr::new("th19netdelayemulate");
+    let mut pipe = if let Ok(pipe) = ByteWriterPipeStream::connect(name) {
+        println!("フック済みのDLLに接続しました");
+        pipe
+    } else {
+        let dll_path = current_exe()?
+            .as_path()
+            .parent()
+            .unwrap()
+            .join(concat!(env!("CARGO_PKG_NAME"), "_hook.dll"));
 
-    inject_dll("th19.exe", &dll_path)?;
+        inject_dll("th19.exe", &dll_path)?;
+
+        let name = OsStr::new("th19netdelayemulate");
+        ByteWriterPipeStream::connect(name).unwrap()
+    };
+
+    let buf = [env::args().nth(1).unwrap().parse::<u8>().unwrap(); 1];
+    pipe.write_all(&buf).unwrap();
 
     Ok(())
 }
