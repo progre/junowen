0820b2ce210fa75f57fd8655837a8d91ae5f1e75
progre <progre+pgp@prgrssv.net>
2023-11-18 12:07:17 +0900

    feat(junowen): more rich progress

--- junowen/src/in_game_lobby/shared_room.rs
+++ junowen/src/in_game_lobby/shared_room.rs
@@ -48,24 +48,34 @@ fn make_leave_menu() -> (u8, CommonMenu) {
     )
 }
 
-fn progress_text(progress: f64) -> Vec<u8> {
+fn progress_text(progress: f64) -> (Vec<u8>, Vec<u8>, Vec<u8>, f64) {
     const LENGTH: f64 = 20.0;
-    let progress = progress * LENGTH % (LENGTH + 1.0);
-    let mut text = vec![b'|'; 1];
-    text.append(&mut vec![b'#'; progress as usize]);
-    let fraction = progress - progress.floor();
+    let progress = ((progress + 1.0) % 2.0 - 1.0) * LENGTH;
+    let mut base_text = vec![b'|'; 1];
+    let mut rear_text = vec![];
+    let mut front_text = vec![];
+    let (progress, left_char, right_char, left_len) = if progress >= 0.0 {
+        (progress, b'#', b'-', progress as usize)
+    } else {
+        let progress = -progress;
+        (progress, b'-', b'#', LENGTH as usize - progress as usize)
+    };
+    let right_len = LENGTH as usize - left_len;
+    base_text.append(&mut vec![left_char; left_len]);
+    rear_text.append(&mut vec![b' '; left_len]);
+    front_text.append(&mut vec![b' '; left_len]);
     if progress < 20.0 {
-        text.push(if fraction < 0.33 {
-            b'-'
-        } else if fraction < 0.66 {
-            b'='
-        } else {
-            b'*'
-        });
+        base_text.push(b' ');
+        rear_text.push(b'-');
+        front_text.push(b'#');
     }
-    text.append(&mut vec![b'-'; (LENGTH - progress) as usize]);
-    text.push(b'|');
-    text
+    base_text.append(&mut vec![right_char; right_len]);
+    rear_text.append(&mut vec![b' '; right_len]);
+    front_text.append(&mut vec![b' '; right_len]);
+    base_text.push(b'|');
+
+    let fraction = progress - progress.floor();
+    (base_text, rear_text, front_text, fraction)
 }
 
 fn render_room_name(th19: &Th19, text_renderer: *const c_void, room_name: &str) {
@@ -88,21 +98,38 @@ fn render_room_name(th19: &Th19, text_renderer: *const c_void, room_name: &str) 
     th19.render_text(text_renderer, &rt);
 }
 
-fn render_progress(th19: &Th19, text_renderer: *const c_void, text: &[u8]) {
+fn render_progress_item(th19: &Th19, text_renderer: *const c_void, alpha: u8, text: &[u8]) {
     let x = (640 * th19.screen_width().unwrap() / 1280) as f32;
     let y = ((160 + 32 * 11) * th19.screen_height().unwrap() / 960) as f32;
     let mut rt = RenderingText::default();
     rt.set_text(text);
     rt.x = x;
     rt.y = y;
-    rt.color = 0xff000000;
+    rt.color = alpha as u32 * 0x1000000;
     rt.font_type = 8;
     rt.horizontal_align = 0;
     th19.render_text(text_renderer, &rt);
 
-    rt.color = 0xffffffff;
+    rt.color = (alpha as u32 * 0x1000000) | 0x00ffffff;
     rt.font_type = 6;
     th19.render_text(text_renderer, &rt);
+}
+
+fn render_progress(th19: &Th19, text_renderer: *const c_void, progress: f64) {
+    let (base_text, rear_text, front_text, fraction) = progress_text(progress);
+    render_progress_item(th19, text_renderer, 0xff, &base_text);
+    render_progress_item(
+        th19,
+        text_renderer,
+        0xff - (0xff as f64 * fraction) as u8,
+        &rear_text,
+    );
+    render_progress_item(
+        th19,
+        text_renderer,
+        (0xff as f64 * fraction) as u8,
+        &front_text,
+    );
 }
 
 pub struct SharedRoom {
@@ -182,8 +209,7 @@ impl SharedRoom {
             } else {
                 elapsed.as_secs_f64() / 4.0
             };
-            let text = progress_text(progress);
-            render_progress(th19, text_renderer, &text);
+            render_progress(th19, text_renderer, progress);
         }
     }
 }
