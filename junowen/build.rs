c33f216a7b901e4de0848fb906200f670a202a5d
progre <progre+pgp@prgrssv.net>
2024-04-16 20:50:42 +0900

    chore: static_vcruntime

--- junowen/build.rs
+++ junowen/build.rs
@@ -1,8 +1,5 @@
-use winres::WindowsResource;
-
 fn main() {
     if cfg!(target_os = "windows") {
         static_vcruntime::metabuild();
-        WindowsResource::new().compile().unwrap();
     }
 }
